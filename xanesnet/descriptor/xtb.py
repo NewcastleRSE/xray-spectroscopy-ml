e8530982ca4e9779d48d0bbc50a8c487f1f52a9b
Tom Penfold <116175928+penfoldtj@users.noreply.github.com>
2024-02-29 10:42:36 +0000

    Complete XTB descriptor included.

--- xanesnet/descriptor/xtb.py
+++ xanesnet/descriptor/xtb.py
@@ -18,19 +18,27 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 ############################### LIBRARY IMPORTS ###############################
 ###############################################################################
 
-from ase import Atoms
 from tblite.interface import Calculator
-from xanesnet.descriptor.base_descriptor import BaseDescriptor
+from ase import Atoms
+import numpy as np
 
+from xanesnet.descriptor import WACSF
 
 ###############################################################################
 ################################## CLASSES ####################################
 ###############################################################################
 
 
-class XTB(BaseDescriptor):
+class XTB(WACSF):
+    """
+    A class for transforming a molecular system into a project density of
+    states representation from XTB model.
+    """
+
     def __init__(
         self,
+        r_min: float = 0.0,
+        r_max: float = 6.0,
         method: str = "GFN2-xTB",
         accuracy: float = 1.0,
         guess: int = 0,
@@ -38,7 +46,20 @@ class XTB(BaseDescriptor):
         mixer_damping: float = 0.4,
         save_integrals: int = 0,
         temperature: float = 9.5e-4,
-        verbosity: int = 1,
+        verbosity: int = 0,
+        e_min: float = -20.0,
+        e_max: float = 20.0,
+        sigma: float = 0.7,
+        num_points: float = 200,
+        use_wacsf=False,
+        n_g2: int = 0,
+        n_g4: int = 0,
+        l: list = [1.0, -1.0],
+        z: list = [1.0],
+        g2_parameterisation: str = "shifted",
+        g4_parameterisation: str = "centred",
+        use_charge=False,
+        use_spin=False,
     ):
         """
         Args:
@@ -54,52 +75,123 @@ class XTB(BaseDescriptor):
                 Defaults to 0 (False).
             temperature (float): Electronic temperature for filling.
                 Defaults to 9.500e-4.
-            verbosity (int): Set verbosity of printout
-                Defaults to 1
+            verbosity (float): Set verbosity of printout
+                Defaults to 0
         """
+        if use_wacsf:
+            super().__init__(
+                r_min,
+                r_max,
+                n_g2,
+                n_g4,
+                l,
+                z,
+                g2_parameterisation,
+                g4_parameterisation,
+                use_charge,
+                use_spin,
+            )
+
+        self.use_wacsf = use_wacsf
+        self.use_spin = use_spin
+        self.use_charge = use_charge
         self.method = method
-        self.accuracy = accuracy
-        self.guess = guess
         self.max_iter = max_iter
-        self.mixer_damping = mixer_damping
-        self.save_integrals = save_integrals
-        self.temperature = temperature
         self.verbosity = verbosity
+        self.e_min = e_min
+        self.e_max = e_max
+        self.num_points = num_points
+        self.sigma = sigma
 
     def transform(self, system: Atoms):
         numbers = system.get_atomic_numbers()
+        nelectron = np.sum(numbers)
         positions = system.get_positions()
-        # Convert atomic unit to bohr
         positions = positions * 1.8897259886
 
-        calc = Calculator(self.method, numbers, positions)
+        if (self.use_spin and not self.use_charge) or (not self.use_spin and self.use_charge):
+            err_str = (
+                "For the p-DOS descriptor, it is not a good idea to only"
+                "consider overall charge or spin state. Both should be"
+                "include simultaneously or not at all."
+            )
+            raise NotImplementedError(err_str)
 
-        calc.set("save-integrals", self.save_integrals)
-        calc.set("accuracy", self.accuracy)
-        calc.set("guess", self.guess)
-        calc.set("max-iter", self.max_iter)
-        calc.set("mixer-damping", self.mixer_damping)
-        calc.set("temperature", self.temperature)
+        else:
+
+            if self.use_spin and self.use_charge:
+                charge = system.info["q"]
+                spin = system.info["s"]
+                if (((nelectron - charge) % 2) == 1) and (spin  % 2) == 0:
+                   err_str = (
+                       "The number of electrons is inconsistent with the spin"
+                       "state you have defined."
+                   )
+                   raise ValueError(err_str)
+                elif (((nelectron - charge) % 2) == 0) and (spin  % 2) == 1:
+                   err_str = (
+                       "The number of electrons is inconsistent with the spin"
+                       "state you have defined."
+                   )
+                   raise ValueError(err_str)
+            else:
+                charge  = 0
+                spin    = 0
+
+        calc = Calculator(self.method, numbers, positions, charge, spin)
         calc.set("verbosity", self.verbosity)
+        calc.set("max-iter", self.max_iter)
 
-        # calc = Calculator(
-        #     method="GFN2-xTB",
-        #     numbers=np.array([14, 1, 1, 1, 1]),
-        #     positions=np.array(
-        #         [
-        #             [0.000000000000, 0.000000000000, 0.000000000000],
-        #             [1.617683897558, 1.617683897558, -1.617683897558],
-        #             [-1.617683897558, -1.617683897558, -1.617683897558],
-        #             [1.617683897558, -1.617683897558, 1.617683897558],
-        #             [-1.617683897558, 1.617683897558, 1.617683897558],
-        #         ]
-        #     ),
-        # )
-        try:
-            res = calc.singlepoint()
-            res.get("energy")  # Results in atomic units
-        except RuntimeError as e:
-            print(f"RuntimeError: {e}")
+        res = calc.singlepoint()
+        res.get("energy") 
+        coeff = res.get("orbital-coefficients")
+        coeff = np.square(coeff)
+        if (numbers[0] >= 21) and (numbers[0] <= 29): 
+            p_dos = np.array([np.sum(coeff[6:8, i]) / np.sum(coeff[:, i]) for i in range(len(coeff))])
+        elif (numbers[0] >= 39) and (numbers[0] <= 47):
+            p_dos = np.array([np.sum(coeff[6:8, i]) / np.sum(coeff[:, i]) for i in range(len(coeff))])
+        elif (numbers[0] >= 57) and (numbers[0] <= 79):
+            p_dos = np.array([np.sum(coeff[6:8, i]) / np.sum(coeff[:, i]) for i in range(len(coeff))])
+        elif (numbers[0] >= 89) and (numbers[0] <= 112):
+            p_dos = np.array([np.sum(coeff[6:8, i]) / np.sum(coeff[:, i]) for i in range(len(coeff))])
+        else:
+            p_dos = np.array([np.sum(coeff[1:3, i]) / np.sum(coeff[:, i]) for i in range(len(coeff))])
+        
+        orbe = np.multiply(res.get("orbital-energies"), 27.211324570273)
+        orbo = res.get("orbital-occupations")
+        lumo = np.where(orbo < 0.001)[0][0]
+        unoccupied_pdos = p_dos[lumo:]
+        unoccupied_orbital_energies = orbe[lumo:]
+
+        # Generate a grid and broaden pDOS
+        x = np.linspace(self.e_min, self.e_max, num=self.num_points, endpoint=True)
+        sigma = self.sigma
+        pdos_gauss = spectrum(unoccupied_orbital_energies, unoccupied_pdos, sigma, x)
+        pdos_gauss = np.multiply(pdos_gauss,100)
+
+        if self.use_wacsf:
+            pdos_gauss = np.append(pdos_gauss, super().transform(system))
+
+        return pdos_gauss
 
     def get_number_of_features(self):
-        return 0
+        if self.use_wacsf:
+            return int(
+                self.num_points
+                + 1
+                + self.n_g2
+                + self.n_g4
+            )
+        else:
+            return int(self.num_points)
+
+def spectrum(E, osc, sigma, x):
+    # This Gaussian broadens the partial density of states over a defined
+    # energy range and grid spacing.
+    gE = []
+    for Ei in x:
+        tot = 0
+        for Ej, os in zip(E, osc):
+            tot += os * np.exp(-((((Ej - Ei) / sigma) ** 2)))
+        gE.append(tot)
+    return gE
